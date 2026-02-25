61fe136daf4363491fd3327af95f10bf29dc933c
Angelo Ferraz Modanez <angelomodanez15@gmail.com>
2026-02-25 19:17:14 -0300

    Revert "feat: header component (#21)" (#22)
    This reverts commit bb364680dc3d284f594333b482eff78f79030c22.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'appointments',
+    loadComponent: () =>
+      import('./features/appointments/pages/home/home')
+        .then(m => m.default)
+  },
+  {
+    path: '',
+    redirectTo: 'appointments',
+    pathMatch: 'full'
+  }
+];
