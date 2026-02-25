f23d4bea9f4b926f0ca90a86e5d4b059db0074e8
Angelo Ferraz Modanez <angelomodanez15@gmail.com>
2026-02-25 17:59:32 -0300

    chore: organize project structure using standalone architecture

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,3 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  {
-    path: 'appointments',
-    loadComponent: () =>
-      import('./features/appointments/pages/home/home')
-        .then(m => m.default)
-  },
-  {
-    path: '',
-    redirectTo: 'appointments',
-    pathMatch: 'full'
-  }
-];
+export const routes: Routes = [];
