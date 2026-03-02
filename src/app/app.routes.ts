656f1b81650dc19b6189c298c69f223f7b6e2ff0
Angelo Ferraz Modanez <angelomodanez15@gmail.com>
2026-03-02 20:48:50 -0300

    chore: add area-page route structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./features/home/home.routes').then(m => m.HOME_ROUTES),
+    path: '',
+    loadChildren: () => import('./features/home/home.routes').then(r => r.HOME_ROUTES),
   },
+  {
+    path: '',
+    loadChildren: () => import('./features/maintenance/maintenance.routes').then(r => r.MAINTENANCE_ROUTES)
+  }
 ];
---
+++ src/app/features/maintenance/maintenance.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+import { AreaPage } from "./pages/area-page/area-page";
+
+export const MAINTENANCE_ROUTES: Routes = [
+  {
+    path: 'area',
+    component: AreaPage
+  }
+]
