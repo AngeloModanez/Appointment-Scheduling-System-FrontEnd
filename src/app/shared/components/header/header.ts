9ab27499d7859d6b115ba0d245e39749f0eac272
Angelo Ferraz Modanez <angelomodanez15@gmail.com>
2026-03-02 20:20:48 -0300

    chore: add home-page route structure

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NgbDropdown, NgbDropdownToggle, NgbDropdownMenu, NgbDropdownItem } from '@ng-bootstrap/ng-bootstrap/dropdown';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-header',
-  imports: [NgbDropdown, NgbDropdownToggle, NgbDropdownMenu, NgbDropdownItem],
+  imports: [NgbDropdown, NgbDropdownToggle, NgbDropdownMenu, NgbDropdownItem, RouterLink],
   templateUrl: './header.html',
-  styles: ``,
+  styleUrl: './header.scss',
 })
 export class Header {
+
 }
