4560d0b3ad959e935451590345b293e7006351fa
Angelo Ferraz Modanez <angelomodanez15@gmail.com>
2026-03-02 20:54:27 -0300

    chore: add professional-page route structure

--- src/app/features/maintenance/maintenance.routes.ts
+++ src/app/features/maintenance/maintenance.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from "@angular/router";
 import { AreaPage } from "./pages/area-page/area-page";
+import { ProfessionalPage } from "./pages/professional-page/professional-page";
 
 export const MAINTENANCE_ROUTES: Routes = [
   {
     path: 'area',
     component: AreaPage
-  }
+  },
+  {
+    path: 'professional',
+    component: ProfessionalPage
+  },
 ]
---
+++ src/app/features/maintenance/pages/professional-page/professional-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-professional-page',
+  imports: [],
+  templateUrl: './professional-page.html',
+  styleUrl: './professional-page.scss',
+})
+export class ProfessionalPage {
+
+}
