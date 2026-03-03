f77b1cf8260f022296a08d7c5748ebf813054bcf
Angelo Ferraz Modanez <angelomodanez15@gmail.com>
2026-03-03 00:10:51 -0300

    chore: add schedule-appointment-page route structure

---
+++ src/app/features/schedule/pages/schedule-appointment-page/schedule-appointment-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-schedule-appointment-page',
+  imports: [],
+  templateUrl: './schedule-appointment-page.html',
+  styleUrl: './schedule-appointment-page.scss',
+})
+export class ScheduleAppointmentPage {
+
+}
--- src/app/features/schedule/schedule.routes.ts
+++ src/app/features/schedule/schedule.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from "@angular/router";
 import { TodayAppointmentPage } from "./pages/today-appointment-page/today-appointment-page";
+import { ScheduleAppointmentPage } from "./pages/schedule-appointment-page/schedule-appointment-page";
 
 export const SCHEDULE_ROUTES: Routes = [
   {
     path: 'today-appointment',
     component: TodayAppointmentPage
+  },
+  {
+    path: 'schedule-appointment',
+    component: ScheduleAppointmentPage
   }
 ]
