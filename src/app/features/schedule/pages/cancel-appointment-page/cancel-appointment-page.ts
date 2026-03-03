b0253b1485bf67b0f66f8bae527a1c37a1b93935
Angelo Ferraz Modanez <angelomodanez15@gmail.com>
2026-03-03 00:13:28 -0300

    chore: add cancel-appointment-page route structure

---
+++ src/app/features/schedule/pages/cancel-appointment-page/cancel-appointment-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cancel-appointment-page',
+  imports: [],
+  templateUrl: './cancel-appointment-page.html',
+  styleUrl: './cancel-appointment-page.scss',
+})
+export class CancelAppointmentPage {
+
+}
--- src/app/features/schedule/schedule.routes.ts
+++ src/app/features/schedule/schedule.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from "@angular/router";
 import { TodayAppointmentPage } from "./pages/today-appointment-page/today-appointment-page";
 import { ScheduleAppointmentPage } from "./pages/schedule-appointment-page/schedule-appointment-page";
+import { CancelAppointmentPage } from "./pages/cancel-appointment-page/cancel-appointment-page";
 
 export const SCHEDULE_ROUTES: Routes = [
   {
@@ -10,5 +11,9 @@ export const SCHEDULE_ROUTES: Routes = [
   {
     path: 'schedule-appointment',
     component: ScheduleAppointmentPage
+  },
+  {
+    path: 'cancel-appointment',
+    component: CancelAppointmentPage
   }
 ]
