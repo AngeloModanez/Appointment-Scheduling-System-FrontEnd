f46fae1416729b85db2fdfcbbe174e7d04c05a73
Angelo Ferraz Modanez <angelomodanez15@gmail.com>
2026-03-13 19:09:46 -0300

    chore: components adjustments

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
-import { NgbDropdown, NgbDropdownToggle, NgbDropdownMenu, NgbDropdownItem } from '@ng-bootstrap/ng-bootstrap/dropdown';
+import { NgbDropdown, NgbDropdownItem, NgbDropdownMenu, NgbDropdownToggle } from '@ng-bootstrap/ng-bootstrap/dropdown';
 import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-header',
-  imports: [NgbDropdown, NgbDropdownToggle, NgbDropdownMenu, NgbDropdownItem, RouterLink],
+  imports: [NgbDropdown, NgbDropdownItem, NgbDropdownMenu, NgbDropdownToggle, RouterLink],
   templateUrl: './header.html',
   styles: ``,
 })
 export class Header {
-
   getName(): string {
     return "John Doe";
   }
