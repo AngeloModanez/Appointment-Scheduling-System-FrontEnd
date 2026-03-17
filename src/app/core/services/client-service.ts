efbba187f2624bd997dc642e5593a3010c7b7525
Angelo Ferraz Modanez <angelomodanez15@gmail.com>
2026-03-16 22:27:51 -0300

    feat: page interface, new get, and update filter input

--- src/app/core/services/client-service.ts
+++ src/app/core/services/client-service.ts
@@ -1,5 +1,5 @@
 import { Client } from "@models/client";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpResponse } from "@angular/common/http";
 import { Injectable, inject } from "@angular/core";
 import { Observable } from "rxjs";
 
@@ -11,9 +11,9 @@ export class ClientService {
 
   baseUrl = "http://localhost:3000/clients"
 
-  getClients(clientNameFilter: string): Observable<Client[]> {
-    let url = `${this.baseUrl}?name_like=${clientNameFilter}`;
-    return this.http.get<Client[]>(url);
+  getClients(nameFilter: string, page: number): Observable<HttpResponse<Client[]>> {
+    let url = `${this.baseUrl}?name_like=${nameFilter}&_page=${page}`;
+    return this.http.get<Client[]>(url, { observe: 'response' });
   }
 
   delete(client: Client): Observable<void> {
--- src/app/features/maintenance/pages/clients-table-page/client-table-page.ts
+++ src/app/features/maintenance/pages/clients-table-page/client-table-page.ts
@@ -2,14 +2,16 @@ import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { Client } from '@models/client';
 import { ClientService } from '@services/client-service';
 import { FormsModule } from '@angular/forms';
+import { NgbPagination } from '@ng-bootstrap/ng-bootstrap/pagination';
 import { PageLayout } from '@components/page-layout/page-layout';
 import { Router } from '@angular/router';
 import { SearchInput } from '@components/search-input/search-input';
 import { Table } from '@components/table/table';
+import { Page } from '@models/page';
 
 @Component({
   selector: 'app-client-table-page',
-  imports: [FormsModule, PageLayout, SearchInput, Table],
+  imports: [FormsModule, NgbPagination, PageLayout, SearchInput, Table],
   templateUrl: './client-table-page.html',
   styles: ``,
 })
@@ -18,29 +20,37 @@ export class ClientTablePage implements OnInit {
   private router = inject(Router);
   private clientService = inject(ClientService);
 
-  clients = signal<Client[]>([]);
-  filter = signal('');
+  clientPage = signal<Page<Client>>({
+    content: [],
+    totalElements: 0
+  });
 
-  filteredClients = computed(() =>
-    this.clients().filter(c =>
-      c.name.toLowerCase().includes(this.filter().toLowerCase())
-    )
-  );
+  page = signal(1);
+  filter = signal('');
 
   ngOnInit(): void {
     this.loadClients();
   }
 
   loadClients() {
-    this.clientService.getClients('').subscribe(client => {
-      this.clients.set(client);
+    this.clientService.getClients(this.filter(), this.page()).subscribe({
+      next: response => {
+        this.clientPage.set({
+          content: response.body ?? [],
+          totalElements: parseInt(response.headers.get("X-Total-Count") || "0")
+        });
+      }
     });
   }
 
-  delete(client: Client) {
-    this.clientService.delete(client).subscribe(() => {
-      this.clients.update(list => list.filter(c => c.id !== client.id));
-    });
+  pageChange() {
+    this.loadClients();
+  }
+
+  filterClients(value: string) {
+    this.filter.set(value);
+    this.page.set(1);
+    this.loadClients();
   }
 
   goToNewClient() {
