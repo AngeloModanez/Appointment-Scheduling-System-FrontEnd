0e81a361f8eb220c3e175aa7b5449018b2b8c741
Angelo Ferraz Modanez <angelomodanez15@gmail.com>
2026-03-27 03:29:34 -0300

    feat: professional model and getProfessionalFromArea method

--- src/app/core/services/area-service.ts
+++ src/app/core/services/area-service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Area } from '@models/area';
+import { Professional } from '@models/professional';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -13,5 +14,10 @@ export class AreaService {
 
   getAreas(): Observable<Area[]> {
     return this.http.get<Area[]>(this.baseUrl);
+  }
+
+  getProfessionalsFromArea(area: Area): Observable<Professional[]> {
+    let url = `${this.baseUrl}/${area.id}/professionals`;
+    return this.http.get<Professional[]>(url);
   }
 }
