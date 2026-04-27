e202b131d5527dadd178f86564bbeb020091bfbd
Angelo Ferraz Modanez <angelomodanez15@gmail.com>
2026-04-27 15:35:28 -0300

    feat: Next and Previous Month

--- src/app/features/schedule/components/calendar/calendar.ts
+++ src/app/features/schedule/components/calendar/calendar.ts
@@ -9,4 +9,31 @@ import { DatePipe, TitleCasePipe } from '@angular/common';
 })
 export class Calendar {
   calendarDate = signal<Date>(new Date());
+
+  onNextMonth() {
+    this.calendarDate.set(new Date(this.calendarDate()))
+    this.calendarDate().setMonth(this.calendarDate().getMonth() + 1);
+    this.calendarDate().setDate(1);
+  }
+
+  onPreviousMonth() {
+    let previousDate = new Date(this.calendarDate())
+    previousDate.setMonth(this.calendarDate().getMonth() - 1);
+    previousDate.setDate(1);
+
+    if (previousDate >= new Date()) {
+      return this.calendarDate.set(previousDate);
+    }
+
+    if (this.isDateCurrentMonthYear(previousDate)) {
+      previousDate.setDate(new Date().getDate());
+      this.calendarDate.set(previousDate);
+    }
+  }
+
+  showPreviousMonth(): boolean {
+    return !this.isDateCurrentMonthYear(this.calendarDate());
+  }
+
+  isDateCurrentMonthYear = (date: Date): boolean => date.getMonth() == new Date().getMonth() && date.getFullYear() == new Date().getFullYear()
 }
