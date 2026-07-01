5a4c57bb501451db3954642edbba10c118cecde1
Angelo Ferraz Modanez <angelomodanez15@gmail.com>
2026-07-01 00:44:50 -0300

    feat: Time using TimSlot and list of availableTimes

---
+++ src/app/features/schedule/components/time/time.ts
@@ -0,0 +1,39 @@
+import { Component, input, signal } from '@angular/core';
+import { TimeModel } from './models/time-model';
+import { TimeSlot } from "@components/time-slot/time-slot";
+import { TimePipe } from "../../../../shared/pipes/time-pipe";
+
+@Component({
+  selector: 'app-time',
+  imports: [TimeSlot, TimePipe],
+  templateUrl: './time.html',
+  styles: ``,
+})
+export class Time {
+  times = signal<TimeModel[]>([]);
+
+  constructor() {
+    this.resetTimes();
+  }
+
+  resetTimes() {
+    this.times.set([
+      {startTime: "08:00:00", endTime: "08:30:00", available: true},
+      {startTime: "08:30:00", endTime: "09:00:00", available: false},
+      {startTime: "09:00:00", endTime: "09:30:00", available: false},
+      {startTime: "09:30:00", endTime: "10:00:00", available: false},
+      {startTime: "10:00:00", endTime: "10:30:00", available: false},
+      {startTime: "10:30:00", endTime: "11:00:00", available: false},
+      {startTime: "11:00:00", endTime: "11:30:00", available: false},
+      {startTime: "11:30:00", endTime: "12:00:00", available: true},
+      {startTime: "14:00:00", endTime: "14:30:00", available: false},
+      {startTime: "14:30:00", endTime: "15:00:00", available: false},
+      {startTime: "15:00:00", endTime: "15:30:00", available: false},
+      {startTime: "15:30:00", endTime: "16:00:00", available: true},
+      {startTime: "16:00:00", endTime: "16:30:00", available: false},
+      {startTime: "16:30:00", endTime: "17:00:00", available: false},
+      {startTime: "17:00:00", endTime: "17:30:00", available: false},
+      {startTime: "17:30:00", endTime: "18:00:00", available: false}
+    ])
+  }
+}
