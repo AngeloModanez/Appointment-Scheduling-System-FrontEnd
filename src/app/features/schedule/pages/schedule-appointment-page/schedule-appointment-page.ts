8928ff51ce4f3e3c20425959dbf6bf63b1a1652b
Angelo Ferraz Modanez <angelomodanez15@gmail.com>
2026-07-17 21:29:03 -0300

    Revert "chore: Change page name route designation"
    This reverts commit 1ff9a7f1cf9d1c354a142f529b33ff94ee626b9e.

---
+++ src/app/features/schedule/pages/schedule-appointment-page/schedule-appointment-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-schedule-appointment-page',
+  imports: [],
+  templateUrl: './schedule-appointment-page.html',
+  styles: ``,
+})
+export class ScheduleAppointmentPage {
+
+}
--- src/app/features/schedule/schedule.routes.ts
+++ src/app/features/schedule/schedule.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from "@angular/router";
+import { ScheduleAppointmentPage } from "./pages/schedule-appointment-page/schedule-appointment-page";
 import { CancelAppointmentPage } from "./pages/cancel-appointment-page/cancel-appointment-page";
 import { ClientHistoryPage } from "./pages/client-history-page/client-history-page";
 import { ProfessionalSchedulePage } from "./pages/professional-schedule-page/professional-schedule-page";
@@ -6,8 +7,12 @@ import { NewAppointment } from "./pages/new-appointment/new-appointment";
 
 export const SCHEDULE_ROUTES: Routes = [
   {
-    path: 'create',
+    path: 'today',
     component: NewAppointment
+  },
+  {
+    path: 'create',
+    component: ScheduleAppointmentPage
   },
   {
     path: 'cancel',
