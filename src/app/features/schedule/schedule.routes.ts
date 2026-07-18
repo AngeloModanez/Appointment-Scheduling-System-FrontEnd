1ff9a7f1cf9d1c354a142f529b33ff94ee626b9e
Angelo Ferraz Modanez <angelomodanez15@gmail.com>
2026-07-17 21:22:58 -0300

    chore: Change page name route designation

--- src/app/features/schedule/schedule.routes.ts
+++ src/app/features/schedule/schedule.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from "@angular/router";
-import { ScheduleAppointmentPage } from "./pages/schedule-appointment-page/schedule-appointment-page";
 import { CancelAppointmentPage } from "./pages/cancel-appointment-page/cancel-appointment-page";
 import { ClientHistoryPage } from "./pages/client-history-page/client-history-page";
 import { ProfessionalSchedulePage } from "./pages/professional-schedule-page/professional-schedule-page";
@@ -7,12 +6,8 @@ import { NewAppointment } from "./pages/new-appointment/new-appointment";
 
 export const SCHEDULE_ROUTES: Routes = [
   {
-    path: 'today',
-    component: NewAppointment
-  },
-  {
     path: 'create',
-    component: ScheduleAppointmentPage
+    component: NewAppointment
   },
   {
     path: 'cancel',
