b2e4c5d3810803c3e72b0710eea8a87ab472c4e5
Angelo Ferraz Modanez <angelomodanez15@gmail.com>
2026-07-29 16:52:46 -0300

    feat: Internacional Phone Validator

--- src/app/features/maintenance/pages/client-form-page/client-form-page.ts
+++ src/app/features/maintenance/pages/client-form-page/client-form-page.ts
@@ -7,6 +7,7 @@ import { ClientService } from '@services/client-service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Client } from '@models/client';
 import { ToastService } from '@services/toast-service';
+import { internationalPhoneValidator } from 'src/app/shared/validators/phone-validator';
 
 @Component({
   selector: 'app-client-form-page',
@@ -25,7 +26,7 @@ export class ClientFormPage {
   clientForm = this.formBuilder.group({
     id: [0],
     name: ['', [Validators.required, Validators.minLength(3)]],
-    phone: ['', [Validators.required]],
+    phone: ['', [Validators.required, internationalPhoneValidator()]],
     dateOfBirth: ['', Validators.required],
   });
 
