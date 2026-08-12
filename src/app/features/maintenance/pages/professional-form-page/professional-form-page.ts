033a719709f995732f5ad150e35732dc70981708
Angelo Ferraz Modanez <angelomodanez15@gmail.com>
2026-08-12 19:12:25 -0300

    feat: Professional Form design

--- src/app/features/maintenance/pages/professional-form-page/professional-form-page.ts
+++ src/app/features/maintenance/pages/professional-form-page/professional-form-page.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { PageLayout } from "@components/page-layout/page-layout";
+import { FormInput } from "@components/form-input/form-input";
+import { Button } from "@components/button/button";
 
 @Component({
   selector: 'app-professional-form-page',
-  imports: [],
+  imports: [PageLayout, FormInput, Button],
   templateUrl: './professional-form-page.html',
   styles: ``,
 })
